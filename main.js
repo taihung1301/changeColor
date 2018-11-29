8dad43becfb2a80fbcb0e4864ced9d7d363a3684
taihung1301 <taihung1301@github.com>
2018-11-29 21:55:06 +0700

    update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var colorRed = document.getElementById('red');
 var colorGreen = document.getElementById('green');
 var colorBlue = document.getElementById('blue');
 var colorText = document.getElementById('title');
+var codesRgb = document.getElementById('codesRgb');
 
 var colorDiv = document.getElementById('colorDiv');
 
@@ -13,6 +14,7 @@ function changeColor(){
 	//var colorDiv = document.getElementById('colorDiv');
 	colorDiv.style.backgroundColor = `rgb(${colorRed.value}, ${colorGreen.value}, ${colorBlue.value})`;
 	colorDiv.style.borderColor = `rgb(${colorRed.value}, ${colorGreen.value}, ${colorBlue.value})`;
+	codesRgb.innerHTML = ' '+ colorRed.value+', '+colorGreen.value+', '+colorBlue.value+' ';
 }
 
 setInterval(changeColor, 1);
